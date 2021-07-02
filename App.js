38014343df066f0384658a98153a119995da7b2e
HbsAdithya <adithyashashith@gmail.com>
2021-07-02 20:21:02 +0530

    VideoHub-Frontend with Video Filtering Function

--- App.js
+++ App.js
@@ -3,7 +3,8 @@ import React from 'react';
 //Screens
 import Login from "./screens/Login";
 import Signup from "./screens/Signup";
-import Welcome from './screens/Welcome'
+import Welcome from './screens/Welcome';
+import Video from './screens/Video';
 
 // React navigation stack
 import RootStack from './navigators/RootStack';
